855e6bfc12f80b222ae169770f9b64be5f33f219
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-11-08 22:59:24 +0700

    added 'update product stock' functionality in connector.py

--- script/database/connector.py
+++ script/database/connector.py
@@ -16,5 +16,31 @@ def add_product(name, category, price=0, description=None):
     connection.commit()
     connection.close()
 
+def _update_product_stock_by(amount, transaction_type, id):
+    if transaction_type not in TRANSACTIONS:
+        raise ValueError("Illegal transaction type")
+    amount = -amount if transaction_type == TRANSACTIONS[PENGELUARAN] else amount
+    sql_statement = "UPDATE barang SET stok=stok+%s WHERE product_id=%s;"
+
+    params = postgre_config()
+    connection = psycopg2.connect(**params)
+    cursor = connection.cursor()
+    cursor.execute(sql_statement, (amount, id))
+
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+def _add_transaction_record():
+    pass
+
+def _add_transaction_detail():
+    pass
+
+def transact():
+    pass
+
 if __name__ == '__main__':
     add_product('pillow', 'kamar', 200000, 'bantal untuk kamar')
+    _update_product_stock_by(5, "PEMASOKAN", 2)
+    _update_product_stock_by(3, "PENGELUARAN", 2)
