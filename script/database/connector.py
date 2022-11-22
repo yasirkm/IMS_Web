f36b915b9cf77f60a7af71095b944b4c647cdd00
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-11-22 23:43:57 +0700

    improved connector

--- script/database/connector.py
+++ script/database/connector.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import psycopg2
 
 from config import postgre_config
@@ -41,56 +39,48 @@ def _update_product_stock_by(product_id, quantity):
     connection.commit()
     connection.close()
 
-def _add_transaction_detail(transaction_id, product_id, quantity):
-    '''
-        Insert a transaction detail
-    '''
 
-    sql_statement = 'INSERT INTO transaction_detail(transaction_id, product_id, quantity) VALUES(%s, %s, %s)'
-
-    connection = psycopg2.connect(**CONNECTION_PARAMS)
-    cursor = connection.cursor()
-    cursor.execute(sql_statement, (transaction_id, product_id, quantity))
-
-    cursor.close()
-    connection.commit()
-    connection.close()
-
-def _add_transaction_record(employee_id, receipt_number, transaction_type):
-    '''
-        Insert a transaction record and return its transaction_id
-    '''
-
-    date_time = datetime.now()
-    date_time = date_time.strftime('%Y-%m-%d %H:%M:%S')
-
-    sql_statement = 'INSERT INTO transaction(employee_id, type, receipt_number, date_time) VALUES(%s, %s, %s, %s) RETURNING transaction_id'
-
-    connection = psycopg2.connect(**CONNECTION_PARAMS)
-    cursor = connection.cursor()
-    cursor.execute(sql_statement, (employee_id, transaction_type, receipt_number, date_time))
-    transaction_id = cursor.fetchone()[0]
-
-    cursor.close()
-    connection.commit()
-    connection.close()
-
-    return transaction_id
-
-def transact(employee_id, receipt_number, transaction_details, transaction_type):
+def transact(employee_id, receipt_number, transaction_details, transaction_type, date_time):
     '''
         Do a transaction. Update the database in accordance to transaction details and transaction type
         
         transaction_details: a tuple of product_id an its quantity
         transaction_type: IN or OUT
     '''
-    transaction_id = _add_transaction_record(employee_id, receipt_number, transaction_type)
+
+    def _add_transaction_detail(transaction_id, product_id, quantity):
+        '''
+            Insert a transaction detail
+        '''
+
+        sql_statement = 'INSERT INTO transaction_detail(transaction_id, product_id, quantity) VALUES(%s, %s, %s)'
+        cursor.execute(sql_statement, (transaction_id, product_id, quantity))
+
+    def _add_transaction_record(employee_id, receipt_number, transaction_type, date_time):
+        '''
+            Insert a transaction record and return its transaction_id
+        '''
+        sql_statement = 'INSERT INTO transaction(employee_id, type, receipt_number, date_time) VALUES(%s, %s, %s, %s) RETURNING transaction_id'
+
+        cursor.execute(sql_statement, (employee_id, transaction_type, receipt_number, date_time))
+        transaction_id = cursor.fetchone()[0]
+
+        return transaction_id
+    
+
+    connection = psycopg2.connect(**CONNECTION_PARAMS)
+    cursor = connection.cursor()
+
+    transaction_id = _add_transaction_record(employee_id, receipt_number, transaction_type, date_time)
 
     for product_id, quantity in transaction_details:
         _add_transaction_detail(transaction_id, product_id, quantity)
         quantity = -quantity if transaction_type == OUT else quantity
         _update_product_stock_by(product_id, quantity)
 
+    cursor.close()
+    connection.commit()
+    connection.close()
     return transaction_id
 
 def get_product_information(product_id, columns=('product_id', 'name', 'category', 'price', 'stock', 'description')):
