acdf6dfba5887d2baa990979a71fa72a06d992f7
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-12-28 08:32:40 +0700

    added login page

--- ims_django/ims_django/urls.py
+++ ims_django/ims_django/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from employee.views import login_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view),
 ]
