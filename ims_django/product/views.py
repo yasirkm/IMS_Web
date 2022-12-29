40fd7d941c7bfc80738f585157e89af284e75aa4
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-12-29 17:23:00 +0700

    added basic templating to catalog view

--- ims_django/product/views.py
+++ ims_django/product/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def catalog_view(request):
-    return render(request, 'catalog.html')
+    catalog = [
+        Product(product_id=1, name='Sepapu Adidas', category='brand', price=100000, stock=20, description='Sepatu bagus'),
+        Product(product_id=2, name='Tas Eiger', category='local', price=300000, stock=3, description='Tas bagus'),
+        ]
+
+    context = {'catalog':catalog}
+    return render(request, 'catalog.html', context)
