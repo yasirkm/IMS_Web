065526288c5b89fddb9f31710d9cadfa9e064ff4
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-01-01 14:34:53 +0700

    updated transaction model

--- ims_django/transaction/models.py
+++ ims_django/transaction/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from employee.models import Employee
 from product.models import Product
@@ -9,6 +11,18 @@ class Transaction(models.Model):
     type = models.TextField(blank=False, null=False)
     receipt_number = models.TextField()
     date_time = models.DateTimeField(blank=False, null=False)
+
+    @classmethod
+    def add_transaction(cls, employee_id, type, receipt_number, transaction_details):
+        date_time = datetime.now()
+        new_transaction = Transaction(employee_id=employee_id, type=type, receipt_number=receipt_number)
+        new_transaction_details = []
+        for product, quantity in transaction_details:
+            new_transaction_details.append(Transaction_Detail(new_transaction, product, quantity))
+
+        new_transaction.save()
+        for new_transaction_detail in new_transaction_details:
+            new_transaction_detail.save()
 
 class Transaction_Detail(models.Model):
     class Meta:
