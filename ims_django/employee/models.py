8d3a40dde4a6e3facc1c391498e8d2cd6353ea1d
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-01-02 06:31:42 +0700

    connected employee to user

--- ims_django/employee/models.py
+++ ims_django/employee/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     employee_id = models.AutoField(primary_key=True)
     username = models.TextField(blank=False, null=False)
     password = models.TextField(blank=False, null=False)
