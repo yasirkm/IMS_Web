bc3db8e0cf225d41549a0eeeafd2554aa63d5875
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-01-02 07:12:09 +0700

    added permissions

--- ims_django/employee/models.py
+++ ims_django/employee/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from product.models import Product
+from transaction.models import Transaction
+
 # Create your models here.
+
+
 class Employee(models.Model):
+    class Meta:
+        permissions = [
+            ("register_user", "Can register another user"),
+        ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     employee_id = models.AutoField(primary_key=True)
     username = models.TextField(blank=False, null=False)
@@ -11,3 +20,69 @@ class Employee(models.Model):
     phone_number = models.TextField(blank=False, null=False)
     address = models.TextField()
     department = models.TextField(blank=False, null=False)
+
+    DEPARTMENT_PERMISSION = {
+        'Management': [
+            'register_user',
+            'view_catalog',
+            'edit_catalog',
+            'view_product_name',
+            'view_product_category',
+            'view_product_description',
+            'view_product_price',
+            'view_product_stock',
+            'edit_product_name',
+            'edit_product_category',
+            'edit_product_description',
+            'edit_product_price',
+            'edit_product_stock',
+            'view_transaction',
+            'do_transaction',
+        ],
+        'Development': [
+            'view_catalog',
+            'view_product_name',
+            'view_product_category',
+            'view_product_description',
+            'view_transaction',
+        ],
+        'Finance': [
+            'view_catalog',
+            'view_product_name',
+            'view_product_category',
+            'view_product_description',
+            'view_product_price',
+            'view_product_stock',
+            'edit_product_price',
+            'view_transaction',
+        ],
+        'Storage': [
+            'view_catalog',
+            'edit_catalog',
+            'view_product_name',
+            'view_product_category',
+            'view_product_description',
+            'view_product_stock',
+            'edit_product_name',
+            'edit_product_category',
+            'edit_product_description',
+            'view_transaction',
+            'do_Transaction'
+
+        ],
+        'Production': [
+            'view_catalog',
+            'view_product_name',
+            'view_product_category',
+            'view_product_description',
+            'view_product_stock',
+        ],
+        'Sales': [
+            'view_catalog',
+            'view_product_name',
+            'view_product_category',
+            'view_product_description',
+            'view_product_price',
+            'view_product_stock',
+        ],
+    }
