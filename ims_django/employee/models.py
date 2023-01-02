ff98bbd68f4b8523c7b4c5cd32e2cf8a5d38f404
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-01-02 07:21:12 +0700

    removed redundant field from employee model

--- ims_django/employee/models.py
+++ ims_django/employee/models.py
@@ -14,9 +14,6 @@ class Employee(models.Model):
         ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     employee_id = models.AutoField(primary_key=True)
-    username = models.TextField(blank=False, null=False)
-    password = models.TextField(blank=False, null=False)
-    name = models.TextField(blank=False, null=False)
     phone_number = models.TextField(blank=False, null=False)
     address = models.TextField()
     department = models.TextField(blank=False, null=False)
